7345d753e7ee985f6b6d586de8df0d897c5e4673
BlackChar <blackcharcz@gmail.com>
2026-02-01 13:00:45 +0100

    working interaction w/ dialog, yay

--- Assets/GameLogic.cs
+++ Assets/GameLogic.cs
@@ -1,3 +1,4 @@
+using cherrydev;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,16 @@ using UnityEngine;
 public class GameLogic : MonoBehaviour
 {
     [SerializeField] private Inventory inventory;
+    [SerializeField] private DialogBehaviour dialogBehaviour;
+    [SerializeField] private DialogNodeGraph dialogGraph;
+
 
     private void Start()
     {
+
+        dialogBehaviour.SetVariableValue("Interaction", 0);
+
+
         if (inventory == null) inventory = FindObjectOfType<Inventory>();
 
         // Demo: try to pick up Mop and Shades at start
@@ -37,4 +45,32 @@ public class GameLogic : MonoBehaviour
         else
             Debug.Log($"Failed to pick up {item} — not obtainable, or inventory full");
     }
+
+
+    public void ItemInteract(GameObject objName)
+    {
+        if (dialogBehaviour != null && objName.name == "HotDogStand")
+        {
+            dialogBehaviour.BindExternalFunction("playAudio", playVO);
+            dialogBehaviour.StartDialog(dialogGraph);
+        }
+    }
+
+    public void playVO()
+    {
+
+        string clipname = dialogBehaviour.GetVariableValue<string>("AudioClip");
+        if (clipname == null || clipname == "")
+        {
+            Debug.Log("No clipname found");
+            return;
+        }
+        AudioClip clip = Resources.Load<AudioClip>("Audio/" + clipname);
+        Debug.Log("Calling playaudio for clipname " + clipname);
+        AudioSource.PlayClipAtPoint(clip, new Vector3(0, 0, 0), 1);
+
+    }
+
+
+
 }
