c3778146beb1be8dcce0621280793f700cc92417
BlackChar <blackcharcz@gmail.com>
2026-02-01 07:08:12 +0100

    Add new assets, working audio playback

--- Assets/Lady_Dialog.cs
+++ Assets/Lady_Dialog.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using cherrydev;
 public class TestDialog : MonoBehaviour
@@ -8,7 +10,20 @@ public class TestDialog : MonoBehaviour
 
     private void Start()
     {
+        dialogBehaviour.BindExternalFunction("playAudio", playVO);
+        dialogBehaviour.SetVariableValue("Interaction", 0);
         dialogBehaviour.StartDialog(dialogGraph);
+
+
+    }
+
+    private void playVO()
+    {
+
+        string clipname = dialogBehaviour.GetVariableValue<string>("AudioClip");
+        AudioClip clip = Resources.Load<AudioClip> ("Audio/"+clipname);
+        Debug.Log("Calling playaudio for clipname " + clipname);
+        AudioSource.PlayClipAtPoint (clip,new Vector3(0, 0, 0), 1);
 
     }
 
